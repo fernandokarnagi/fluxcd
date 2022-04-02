d332ebffe484cb2839073eea07062dc5430a4219
Fernando Karnagi <fernando@cyder.com.sg>
2022-04-02 20:27:51 +0800

    Updated scripts

---
+++ cmd/flux_resume.sh
@@ -0,0 +1,4 @@
+export GITHUB_TOKEN=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
+export GITHUB_USER=fernandokarnagi
+
+flux --kubeconfig kubeconfig/kubeconfig.yml --insecure-skip-tls-verify resume podinfo	master/b891025 
