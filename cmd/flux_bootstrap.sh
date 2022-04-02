5d6a50ecac59bf7df2fcfb76852e3b46b88fe62c
Fernando Karnagi <fernando@cyder.com.sg>
2022-04-02 21:35:21 +0800

    update

--- cmd/flux_bootstrap.sh
+++ cmd/flux_bootstrap.sh
@@ -1,4 +1,1 @@
-export GITHUB_TOKEN=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
-export GITHUB_USER=fernandokarnagi
-
 flux --kubeconfig kubeconfig/kubeconfig.yml --insecure-skip-tls-verify bootstrap github --owner=$GITHUB_USER --repository=fleet-infra --branch=main --path=./clusters/my-cluster --personal
--- cmd/flux_resume.sh
+++ cmd/flux_resume.sh
@@ -1,4 +1,1 @@
-export GITHUB_TOKEN=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
-export GITHUB_USER=fernandokarnagi
-
 flux --kubeconfig kubeconfig/kubeconfig.yml --insecure-skip-tls-verify resume podinfo	master/b891025 
